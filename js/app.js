b5d9d4474bc08201c3cce7acff9d28d2ccb6c2b0
timothee <timothee.hatton@gmail.com>
2016-06-07 13:08:00 +0200

    first-update

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+  //cover
+  function HeightBackground(){
+  	var height = $(window).height();
+  	$("#cover").css({
+  		'height': height
+  	});
+  }
+  HeightBackground();
+  $(window).resize(function(){
+  	HeightBackground();
+  });
+  //parallax
+  function parallax() {
+    $('.banner--primary').parallax("50%", 0.2);
+  }
+  if ($(window).width() > 800){
+    parallax();
+  }
+  // menu Responsive
+  function menuResponsive(){
+    $('#btn-nav').click(function(){
+      $('.header').toggleClass('header-active');
+      $('.header--btn--line').toggleClass('header--btn--line-active');
+    });
+  }
+  menuResponsive();
+  // masonry
+  function masonryInit(){
+    new AnimOnScroll( document.getElementById( 'grid' ), {
+      minDuration : 0.4,
+      maxDuration : 0.7,
+      viewportFactor : 0.2
+    } );
+  }
+  masonryInit();
+});
