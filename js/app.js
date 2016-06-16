dc8e8200a21b1255b2da55138c538238546c69c9
timothee <timothee.hatton@gmail.com>
2016-06-16 17:14:34 +0200

    update

--- js/app.js
+++ js/app.js
@@ -26,12 +26,12 @@ $( document ).ready(function() {
   }
   menuResponsive();
   // masonry
-  function masonryInit(){
+  function masonryInit(){ 
     new AnimOnScroll( document.getElementById( 'grid' ), {
       minDuration : 0.4,
       maxDuration : 0.7,
       viewportFactor : 0.2
-    } );
+    });
   }
   masonryInit();
 });
