6752dce4f63edfd4158c7d63409d3ad810b968b6
timothee <timothee.hatton@gmail.com>
2016-06-23 00:28:51 +0200

    desktop update

--- js/app.js
+++ js/app.js
@@ -13,11 +13,14 @@ $( document ).ready(function() {
   //parallax
   function parallax() {
     $('.banner--primary').parallax("50%", 0.2);
+    $('#parallaxImg1').parallax("50%", 0.1);
+    $('#parallaxImg2').parallax("50%", 0);
+    $('#parallaxImg3').parallax("50%", 0.8);
   }
   if ($(window).width() > 800){
     parallax();
   }
-  // menu Responsive
+  //menu Responsive
   function menuResponsive(){
     $('#btn-nav').click(function(){
       $('.header').toggleClass('header-active');
@@ -25,8 +28,21 @@ $( document ).ready(function() {
     });
   }
   menuResponsive();
-  // masonry
-  function masonryInit(){ 
+  //menu effect
+  function menuEffect() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() >= 200 && !$('.header').hasClass('none')) {
+        $('.header').addClass('header-activeEffect');
+        $('.header--logo').addClass('header--logo-activeEffect');
+      } else {
+        $('.header').removeClass('header-activeEffect');
+        $('.header--logo').removeClass('header--logo-activeEffect');
+      }
+    });
+  }
+  menuEffect();
+  //masonry
+  function masonryInit(){
     new AnimOnScroll( document.getElementById( 'grid' ), {
       minDuration : 0.4,
       maxDuration : 0.7,
@@ -34,4 +50,16 @@ $( document ).ready(function() {
     });
   }
   masonryInit();
+  //imgZoom
+  function imgZoom() {
+    $('.lesToiles--masonry--item--img').click(function(){
+      $(this).toggleClass('lesToiles--masonry--item--img-active');
+      $('.zoom--background').fadeToggle(250);
+    });
+    $('.zoom--background').click(function(){
+      $('.lesToiles--masonry--item--img').removeClass('lesToiles--masonry--item--img-active');
+      $('.zoom--background').fadeOut(250);
+    });
+  }
+  imgZoom();
 });
